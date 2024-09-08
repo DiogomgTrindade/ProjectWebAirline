fea223b662df98ecb90fb23d8620f8bc5a9ffd99
DiogomgTrindade <diogovsky1904@gmail.com>
2024-09-08 22:14:13 +0100

    API Aircraft controller

---
+++ ProjectWebAirlineMVC/Controllers/API/AircraftController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProjectWebAirlineMVC.Data;
+
+namespace ProjectWebAirlineMVC.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AircraftController : ControllerBase
+    {
+
+        private readonly IAircraftRepository _aircraftRepository;
+
+        public AircraftController(IAircraftRepository aircraftRepository)
+        {
+
+            _aircraftRepository = aircraftRepository;
+        }
+
+
+        [HttpGet]
+        public IActionResult GetAircrafts()
+        {
+            return Ok(_aircraftRepository.GetAll());
+        }
+
+    }
+}
